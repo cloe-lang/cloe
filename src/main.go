ffe3d45914c40d806ff518764bcd16372a3e1323
Yota Toyama <raviqqe@gmail.com>
2016-12-20 00:30:57 +0900

    Add options

--- src/main.go
+++ src/main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"github.com/docopt/docopt-go"
+)
+
 func main() {
+	usage := `Risp interpreter
+
+Usage:
+  risp <filename>
+
+Options:
+  -h --help     Show this help.`
+
+	args, _ := docopt.Parse(usage, nil, true, "Risp 0.0.0", false)
+	fmt.Println(args)
 }

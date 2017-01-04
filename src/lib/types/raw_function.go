64183c064bb9e75323a7c4c4115cf36f57e1dd20
Yota Toyama <raviqqe@gmail.com>
2017-01-04 14:36:44 +0900

    Add dirty comment

--- src/lib/types/raw_function.go
+++ src/lib/types/raw_function.go
@@ -1,7 +1,28 @@
 package types
 
-type RawFunction func(Dictionary) Object
+type RawFunction func(Dictionary) Object // Environment -> Object
 
 func (f RawFunction) Call(args Dictionary) Object {
 	return f(args)
 }
+
+// func CompileFunction(o Object) (RawFunction, error) {
+// 	os := o.(List).Slice()
+
+// 	if !len(os) != 3 {
+// 		return nil, Error(
+// 			"Invalid number of elements in a list representing a function. %#v", os)
+// 	}
+
+// 	args := os[1]
+// 	body := os[2]
+
+// 	return func(env Dictionary) Object {
+// 		if v, ok := Dictioanary.Get(); ok {
+
+// 		} else {
+// 		}
+
+// 		return
+// 	}
+// }

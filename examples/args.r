671182f06db8181a8fb53985476c9b22cb5f2e6e
Yota Toyama <raviqqe@gmail.com>
2017-01-04 23:27:55 +0900

    Modify args example

--- examples/args.r
+++ examples/args.r
@@ -1,8 +1,7 @@
 (let kwargs {'y5 123 'y6 456})
+(let args '(1 2 3 4 "foo-bar-baz"))
 
-((\ (x1 x2 (x3 123) (x4 456) args.. y1 (y2 123) y3 (y4 456) kwargs...) x)
- 1 2 3 4 list.. . y1 123 y3 456 kwargs...)
+; This is comment!!!
 
-; We don't have to support neither `*[123 456]` nor `**{'y5 123 'y6 456}`
-; because they can just be expanded into arguments directly.
-; (e.g. `(func 123 456 * y5 123 y6 456)`)
+((\ (x1 x2 (x3 123) (x4 456) args.. y1 (y2 123) y3 (y4 456) kwargs...) 42)
+ 1 2 3 4 args.. y1 123 y3 456 kwargs...)

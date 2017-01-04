b53225dde21ab4656ec2b5abbf5ac3d14e543eb7
Yota Toyama <raviqqe@gmail.com>
2017-01-04 19:15:43 +0900

    Make List lazy

--- src/lib/vm/list.go
+++ src/lib/vm/list.go
@@ -1,15 +1,51 @@
 package vm
 
-import "github.com/mediocregopher/seq"
+type List struct {
+	first *Thunk
+	rest  *Thunk
+}
 
-type List struct{ *seq.List }
+func NewList(ts ...*Thunk) *Thunk {
+	list := Cons(ts[len(ts)-1], Nil())
 
-func NewList(ts ...*Thunk) List {
-	new := make([]interface{}, len(ts))
-
-	for i, t := range ts {
-		new[i] = t
+	for i := len(ts) - 2; i >= 0; i-- {
+		list = Cons(ts[i], list)
 	}
 
-	return List{seq.NewList(new...)}
+	return list
+}
+
+func Cons(t1, t2 *Thunk) *Thunk {
+	return Normal(List{t1, t2})
+}
+
+func First(t *Thunk) *Thunk {
+	return applyList(func(l List) *Thunk { return l.first }, t)
+}
+
+func Rest(t *Thunk) *Thunk {
+	return applyList(func(l List) *Thunk { return l.rest }, t)
+}
+
+func applyList(f func(List) *Thunk, t *Thunk) *Thunk {
+	o := evalList(t)
+
+	if l, ok := o.(List); ok {
+		return f(l)
+	}
+
+	return Normal(o)
+}
+
+func evalList(t *Thunk) Object {
+	t.Eval()
+	t.Wait()
+
+	list, ok := t.Result.(List)
+
+	if !ok {
+		return NewError("Expected List but %#v.", t.Result)
+	}
+
+	return list
 }

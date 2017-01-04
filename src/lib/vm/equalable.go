bae345c2eac4bfffb605789358414e71dc2e5333
Yota Toyama <raviqqe@gmail.com>
2017-01-04 22:46:52 +0900

    Merge callable.go and equalable.go into interfaces.go

--- src/lib/vm/equalable.go
+++
@@ -1,5 +0,0 @@
-package vm
-
-type Equalable interface {
-	Equal(t1, t2 *Thunk) *Thunk
-}
--- src/lib/vm/interfaces.go
+++ src/lib/vm/interfaces.go
@@ -3,3 +3,7 @@ package vm
 type Callable interface {
 	Call(List) *Thunk
 }
+
+type Equalable interface {
+	Equal(t1, t2 *Thunk) *Thunk
+}

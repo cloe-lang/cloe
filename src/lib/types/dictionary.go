4ef110e23bd7dba3d78a46f3691c493c7b85b868
Yota Toyama <raviqqe@gmail.com>
2017-01-04 13:06:46 +0900

    Modify NewDictionary

--- src/lib/types/dictionary.go
+++ src/lib/types/dictionary.go
@@ -2,34 +2,18 @@ package types
 
 import "github.com/mediocregopher/seq"
 
-type Dictionary seq.HashMap
+type Dictionary struct{ *seq.HashMap }
 
-func NewDictionary(args Dictionary) Object {
-	o, ok := args.Get(NewString("key_value_pairs"))
+func NewDictionary(os ...Object) (*Dictionary, error) {
+	d := &Dictionary{seq.NewHashMap()}
 
-	if !ok {
-		panic(`An argument, "key_value_pairs" not found.`)
+	if len(os)%2 != 0 {
+		return nil, NewError("Number of arguments is not even.")
 	}
 
-	a, ok := o.(Array)
-
-	if !ok {
-		panic(`Invalid type for an argument "key_value_pairs".`)
+	for i := 0; i < len(os); i += 2 {
+		d.Set(os[i], os[i+1])
 	}
 
-	if len(a)%2 != 0 {
-		panic("Number of arguments is not even.")
-	}
-
-	h := seq.NewHashMap()
-
-	for i := 0; i < len(a); i += 2 {
-		h.Set(a[i], a[i+1])
-	}
-
-	return (*Dictionary)(h)
-}
-
-func (d *Dictionary) Get(k Object) (Object, bool) {
-	return (*seq.HashMap)(d).Get(k)
+	return d, nil
 }

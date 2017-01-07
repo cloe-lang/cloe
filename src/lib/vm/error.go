16f44d35ab14a1f81ce7fe42095c50d8bac39efb
Yota Toyama <raviqqe@gmail.com>
2017-01-07 15:27:54 +0900

    Add TODO of error package

--- src/lib/vm/error.go
+++ src/lib/vm/error.go
@@ -8,6 +8,11 @@ func NewError(s string, xs ...interface{}) *Thunk {
 	return Normal(Error(fmt.Sprintf(s, xs...)))
 }
 
+func ChainError(e *Thunk, s string, xs ...interface{}) *Thunk {
+	// TODO: Error { name string, messsage string, child *Thunk }
+	return nil
+}
+
 func isError(o Object) bool {
 	_, ok := o.(Error)
 	return ok

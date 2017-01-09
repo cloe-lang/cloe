46337e1ebd0bfb474f1bb6cf5c9f3ddd7a9ff92a
Yota Toyama <raviqqe@gmail.com>
2017-01-10 02:10:51 +0900

    Implement Y

---
+++ src/lib/vm/y.go
@@ -0,0 +1,18 @@
+package vm
+
+var Y = NewLazyFunction(y)
+
+func y(ts ...*Thunk) Object {
+	if len(ts) != 1 {
+		return NumArgsError("y", "1")
+	}
+
+	xfxx := Normal(Partial(Normal(fxx), ts[0]))
+	return App(xfxx, xfxx).Eval()
+}
+
+var fxx = NewLazyFunction(fxxImpl)
+
+func fxxImpl(ts ...*Thunk) Object {
+	return Partial(ts[0], App(ts[1], ts[1]))
+}
---
+++ src/lib/vm/y_test.go
@@ -0,0 +1,36 @@
+package vm
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestY(t *testing.T) {
+	for _, n := range []float64{0, 1, 2, 3, 4, 5, 6, 100} {
+		n1 := float64(Y(Normal(NewLazyFunction(lazyFactorial))).(Callable).Call(NumberThunk(n)).(Number))
+		n2 := strictFactorial(n)
+
+		fmt.Printf("%d: %f == %f?\n", int(n), n1, n2)
+
+		if n1 != n2 {
+			t.Fail()
+		}
+	}
+}
+
+func strictFactorial(n float64) float64 {
+	if n == 0 {
+		return 1
+	}
+
+	return n * strictFactorial(n-1)
+}
+
+func lazyFactorial(ts ...*Thunk) Object {
+	return If(
+		App(Normal(Equal), ts[1], NumberThunk(0)),
+		NumberThunk(1),
+		App(Normal(Mult),
+			ts[1],
+			App(ts[0], App(Normal(Sub), ts[1], NumberThunk(1)))))
+}

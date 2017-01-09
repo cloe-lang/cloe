c76e444a00f4e2bf177c7b929ba9f2fc916a0d60
Yota Toyama <raviqqe@gmail.com>
2017-01-10 02:49:53 +0900

    Test passing multiple arguments to Y f

--- src/lib/vm/y_test.go
+++ src/lib/vm/y_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestY(t *testing.T) {
 	for _, n := range []float64{0, 1, 2, 3, 4, 5, 6, 100} {
-		n1 := float64(Y(Normal(NewLazyFunction(lazyFactorial))).(Callable).Call(NumberThunk(n)).(Number))
+		n1 := lazyFactorial(NumberThunk(n))
 		n2 := strictFactorial(n)
 
 		t.Logf("%d: %f == %f?\n", int(n), n1, n2)
@@ -12,6 +12,14 @@ func TestY(t *testing.T) {
 		if n1 != n2 {
 			t.Fail()
 		}
+	}
+
+	for _, ts := range [][]*Thunk{
+		{NumberThunk(7)},
+		{NumberThunk(13), StringThunk("foobarbaz")},
+		{NumberThunk(42), NilThunk(), NilThunk()},
+	} {
+		t.Log(lazyFactorial(ts...))
 	}
 }
 
@@ -23,11 +31,17 @@ func strictFactorial(n float64) float64 {
 	return n * strictFactorial(n-1)
 }
 
-func lazyFactorial(ts ...*Thunk) Object {
+func lazyFactorial(ts ...*Thunk) float64 {
+	return float64(Y(Normal(NewLazyFunction(lazyFactorialImpl))).(Callable).Call(ts...).(Number))
+}
+
+func lazyFactorialImpl(ts ...*Thunk) Object {
+	// fmt.Println(len(ts))
+
 	return If(
 		App(Normal(Equal), ts[1], NumberThunk(0)),
 		NumberThunk(1),
 		App(Normal(Mult),
 			ts[1],
-			App(ts[0], App(Normal(Sub), ts[1], NumberThunk(1)))))
+			App(ts[0], append([]*Thunk{App(Normal(Sub), ts[1], NumberThunk(1))}, ts[2:]...)...)))
 }

163390b1e67a76621575de32cda9e99faad60b16
Yota Toyama <raviqqe@gmail.com>
2017-01-14 23:06:56 +0900

    Add IR compiler

---
+++ src/lib/vm/compile.go
@@ -0,0 +1,26 @@
+package vm
+
+import "fmt"
+
+func Compile(ir interface{}) *Thunk {
+	return NewLazyFunction(func(ts ...*Thunk) Object {
+		return compileIR(ts, ir)
+	})
+}
+
+func compileIR(args []*Thunk, ir interface{}) *Thunk {
+	switch x := ir.(type) {
+	case int:
+		return args[x]
+	case []interface{}:
+		ts := make([]*Thunk, 0, len(x))
+
+		for _, expr := range x {
+			ts = append(ts, compileIR(args, expr))
+		}
+
+		return App(ts[0], ts[1:]...)
+	default:
+		panic(fmt.Sprintf("Invalid type in IR. {}", x))
+	}
+}
---
+++ src/lib/vm/compile_test.go
@@ -0,0 +1,18 @@
+package vm
+
+import "testing"
+
+func TestCompile(t *testing.T) {
+	var n1, n2, n3 float64 = 123, 456, 789
+
+	f := Compile([]interface{}{0, 1, []interface{}{0, 2, 3}})
+
+	x1 := float64(App(f, Add, NewNumber(n1), NewNumber(n2), NewNumber(n3)).Eval().(numberType))
+	x2 := n1 + n2 + n3
+
+	t.Logf("%f == %f?", x1, x2)
+
+	if x1 != x2 {
+		t.Fail()
+	}
+}

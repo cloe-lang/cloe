656aa03492b550228b77fe8fdf703fa23d659b9e
Yota Toyama <raviqqe@gmail.com>
2017-01-15 15:15:39 +0900

    Test Ys with mutually recursive functions

--- src/lib/vm/y_test.go
+++ src/lib/vm/y_test.go
@@ -46,7 +46,7 @@ func lazyFactorialImpl(ts ...*Thunk) Object {
 			App(ts[0], append([]*Thunk{App(Sub, ts[1], NewNumber(1))}, ts[2:]...)...)))
 }
 
-func TestYs(t *testing.T) {
+func TestYsSingleF(t *testing.T) {
 	fs := App(Ys, NewLazyFunction(lazyFactorialImpl)).Eval().([]*Thunk)
 
 	for _, n := range []float64{0, 1, 2, 3, 4, 5, 6, 100} {
@@ -56,6 +56,43 @@ func TestYs(t *testing.T) {
 		t.Logf("%d: %f == %f?\n", int(n), n1, n2)
 
 		if n1 != n2 {
+			t.Fail()
+		}
+	}
+}
+
+func TestYsMultipleFs(t *testing.T) {
+	even := NewLazyFunction(func(ts ...*Thunk) Object {
+		n := ts[2]
+
+		return App(If,
+			App(Equal, n, NewNumber(0)),
+			True,
+			App(ts[1], App(Sub, n, NewNumber(1))))
+	})
+
+	odd := NewLazyFunction(func(ts ...*Thunk) Object {
+		n := ts[2]
+
+		return App(If,
+			App(Equal, n, NewNumber(0)),
+			False,
+			App(ts[0], App(Sub, n, NewNumber(1))))
+	})
+
+	fs := App(Ys, even, odd).Eval().([]*Thunk)
+
+	for _, n := range []float64{0, 1, 2, 3, 4, 5, 6, 100, 121, 256, 1023} {
+		b1 := bool(App(fs[0], NewNumber(n)).Eval().(boolType))
+		b2 := bool(App(fs[1], NewNumber(n)).Eval().(boolType))
+
+		t.Logf("n = %v, even? %v, odd? %v\n", n, b1, b2)
+
+		if b1 != (int(n)%2 == 0) {
+			t.Fail()
+		}
+
+		if b2 != (int(n)%2 != 0) {
 			t.Fail()
 		}
 	}

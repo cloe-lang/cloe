8da4521546f742c50fc66147b38cc690d0d26270
Yota Toyama <raviqqe@gmail.com>
2017-01-16 14:03:30 +0900

    Implement Dictionary.Get()

--- src/lib/vm/dictionary.go
+++ src/lib/vm/dictionary.go
@@ -21,15 +21,33 @@ func NewDictionary(ks []Object, vs []*Thunk) *Thunk {
 }
 
 func (d dictionaryType) Set(k Object, v *Thunk) Object {
-	if s, ok := k.(seq.Setable); ok {
-		h, _ := d.hashMap.Set(s, v)
-		return dictionaryType{h}
+	if _, ok := k.(seq.Setable); !ok {
+		return notSetableError(k)
 	}
 
-	return typeError(k, "Setable")
+	h, _ := d.hashMap.Set(k, v)
+	return dictionaryType{h}
+}
+
+func (d dictionaryType) Get(k Object) *Thunk {
+	if _, ok := k.(seq.Setable); !ok {
+		return notSetableError(k)
+	}
+
+	if v, ok := d.hashMap.Get(k); ok {
+		return v.(*Thunk)
+	}
+
+	return internalError(
+		"KeyNotFoundError",
+		"The key %v is not found in a dictionary.", k)
 }
 
 func (d1 dictionaryType) equal(e equalable) Object {
 	// TODO: Use ToList and compare them as Lists
 	return rawBool(d1.hashMap.Equal(e.(dictionaryType).hashMap))
+}
+
+func notSetableError(k Object) *Thunk {
+	return typeError(k, "Setable")
 }
--- src/lib/vm/dictionary_test.go
+++ src/lib/vm/dictionary_test.go
@@ -1,15 +1,61 @@
 package vm
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestDictionarySet(t *testing.T) {
 	for _, th := range []*Thunk{
 		True, False, Nil, NewNumber(42), NewString("risp"),
 	} {
 		_, ok := EmptyDictionary.Eval().(dictionaryType).Set(th.Eval(), Nil).(dictionaryType)
+		assert.True(t, ok)
+	}
+}
 
-		if !ok {
-			t.Fail()
+func TestDictionaryGet(t *testing.T) {
+	for _, kvs := range [][][]*Thunk{
+		{{True, False}},
+		{{Nil, NewNumber(42)}},
+		{{True, False}, {False, NewNumber(42)}},
+		{{True, False}, {False, NewNumber(42)}, {Nil, NewString("Mr. Value")}},
+	} {
+		d := EmptyDictionary.Eval().(dictionaryType)
+
+		for i, kv := range kvs {
+			t.Logf("Setting a %vth key...\n", i)
+
+			k, v := extractKeyAndValue(kv)
+
+			var ok bool
+			d, ok = d.Set(k, v).(dictionaryType)
+
+			assert.True(t, ok)
+		}
+
+		assert.Equal(t, len(kvs), int(d.hashMap.Size()))
+
+		for i, kv := range kvs {
+			t.Logf("Getting a %vth value...\n", i)
+
+			k, v := extractKeyAndValue(kv)
+
+			t.Log(k)
+
+			if e, ok := d.Get(k).Eval().(errorType); ok {
+				t.Log(e.message.Eval())
+			}
+
+			assert.True(t, bool(App(Equal, d.Get(k), v).Eval().(boolType)))
 		}
 	}
+}
+
+func extractKeyAndValue(kv []*Thunk) (Object, *Thunk) {
+	if len(kv) != 2 {
+		panic("Invalid number of arguments to extractKeyAndValue.")
+	}
+
+	return kv[0].Eval(), kv[1]
 }

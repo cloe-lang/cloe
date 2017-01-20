9b730e7ed447e170ca3c8523b23945905476240c
Yota Toyama <raviqqe@gmail.com>
2017-01-20 22:51:20 +0900

    Remove ,

--- examples/args.r
+++ examples/args.r
@@ -1,4 +1,4 @@
-; . after ..args is not necessary. But, it can be forced for readability.
+; . after ..args is not necessary. But it can be forced for readability.
 (def (func x1 x2 (x3 123) (x4 456) ..args . y1 (y2 123) y3 (y4 456) ..kwargs)
      (+ x1 x2 x3 x4))
 

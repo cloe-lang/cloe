2bc07cfc8a513a0b56f375b031fb4f6412ea13e2
Yota Toyama <raviqqe@gmail.com>
2017-01-21 14:44:10 +0900

    Improve def example

--- examples/def.r
+++ examples/def.r
@@ -1,3 +1,10 @@
 (def foo 123)
 (def bar 456)
+
+(def (func x y)
+  "This function calculate (x + y)^3 + (x + y)^2 + (x + y)^1"
+  "It should be very useful."
+  (def z (+ x y))
+  (+ (^ z 3) (^ z 2) z))
+
 (print (+ foo bar))

841d90a9a2befeaf4b384a818fe3d460fa2fddf3
Yota Toyama <raviqqe@gmail.com>
2017-01-23 21:57:33 +0900

    Add constructors of instructions

--- src/lib/ir/instructions.go
+++ src/lib/ir/instructions.go
@@ -5,13 +5,25 @@ type LetConst struct {
 	expr Expression
 }
 
+func NewLetConst(s string, e Expression) LetConst {
+	return LetConst{name: s, expr: e}
+}
+
 type LetFunction struct {
 	name string
 	body Expression
 }
 
+func NewLetFunction(s string, e Expression) LetFunction {
+	return LetFunction{name: s, body: e}
+}
+
 type Output struct {
 	expr Expression
+}
+
+func NewOutput(e Expression) Output {
+	return Output{e}
 }
 
 // For LetConst and Output,

bd6418ea5cdc71f8c601d03102a245a8b8f36af9
Yota Toyama <raviqqe@gmail.com>
2017-01-26 00:49:56 +0900

    Dump tree in test

--- src/lib/rbt/rbt_test.go
+++ src/lib/rbt/rbt_test.go
@@ -1,6 +1,7 @@
 package rbt
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -9,6 +10,27 @@ type key int
 
 func (k key) Less(o Ordered) bool {
 	return k < o.(key)
+}
+
+func (n *node) dump(t *testing.T) {
+	n.dumpWithIndent(t, 0)
+}
+
+func (n *node) dumpWithIndent(t *testing.T, i int) {
+	for j := 0; j < i; j++ {
+		fmt.Printf(" ")
+	}
+
+	if n == nil {
+		fmt.Println(nil)
+		return
+	}
+
+	fmt.Println(n.value)
+
+	k := i + 2
+	n.left.dumpWithIndent(t, k)
+	n.right.dumpWithIndent(t, k)
 }
 
 func TestNode(t *testing.T) {
@@ -22,12 +44,14 @@ func TestNode(t *testing.T) {
 	assert.Equal(t, n.search(k), k)
 }
 
-func TestTreeConstruction(t *testing.T) {
-	ks := []key{1, 2, 3}
+func TestNodeBalance(t *testing.T) {
+	ks := []key{1, 2, 3, 4, 5, 6}
 	n := (*node)(nil)
 
 	for _, k := range ks {
-		t.Log(n)
+		n.dump(t)
 		n = n.insert(k)
 	}
+
+	n.dump(t)
 }

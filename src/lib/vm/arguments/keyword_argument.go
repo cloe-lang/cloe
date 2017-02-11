42efd50a169ed6a93015b81779d816beb4838629
Yota Toyama <raviqqe@gmail.com>
2017-02-11 16:29:38 +0900

    s/key/name/g

--- src/lib/vm/arguments/keyword_argument.go
+++ src/lib/vm/arguments/keyword_argument.go
@@ -3,10 +3,10 @@ package signature
 import ".."
 
 type KeywordArgument struct {
-	key   string
+	name  string
 	value *vm.Thunk
 }
 
-func NewKeywordArgument(key string, value *vm.Thunk) KeywordArgument {
-	return KeywordArgument{key: key, value: value}
+func NewKeywordArgument(name string, value *vm.Thunk) KeywordArgument {
+	return KeywordArgument{name: name, value: value}
 }

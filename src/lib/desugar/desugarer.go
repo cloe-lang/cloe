059629c5a3b5821c71fdbc0bb5a4919c82856ae4
Yota Toyama <raviqqe@gmail.com>
2017-03-02 00:27:15 +0900

    Modify prototype of desugarer

--- src/lib/desugar/desugarer.go
+++ src/lib/desugar/desugarer.go
@@ -1,23 +1,33 @@
 package desugar
 
-type desugarer struct{}
+import "github.com/raviqqe/tisp/src/lib/ast"
+
+type desugarer struct {
+	statements []interface{}
+}
 
 func newDesugarer() desugarer {
 	return desugarer{}
 }
 
 func (d *desugarer) desugar(module []interface{}) []interface{} {
-	ss := make([]interface{}, 0, 2*len(module)) // TODO: Best cap?
+	d.statements = make([]interface{}, 0, 2*len(module)) // TODO: Best cap?
 
 	for _, s := range module {
-		ss = append(ss, d.desugarStatement(s))
+		d.appendStatement(d.desugarStatement(s))
 	}
 
-	return ss
+	return d.statements
 }
 
-func (*desugarer) desugarStatement(s interface{}) interface{} {
+func (d *desugarer) appendStatement(s interface{}) {
+	d.statements = append(d.statements, s)
+}
+
+func (d *desugarer) desugarStatement(s interface{}) interface{} {
 	switch s := s.(type) {
+	case ast.LetFunction:
+		return s
 	default:
 		return s
 	}

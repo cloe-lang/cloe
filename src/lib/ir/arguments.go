ea7dcdefbe58b4e5aa371efae733887d2df75a2e
Yota Toyama <raviqqe@gmail.com>
2017-03-11 22:26:16 +0900

    Add more documents

--- src/lib/ir/arguments.go
+++ src/lib/ir/arguments.go
@@ -1,11 +1,14 @@
 package ir
 
+// Arguments represents arguments passed to a function in IR.
 type Arguments struct {
 	positionals   []PositionalArgument
 	keywords      []KeywordArgument
 	expandedDicts []interface{}
 }
 
+// NewArguments creates arguments from positional and keyword arguments and
+// expanded dictionaries.
 func NewArguments(ps []PositionalArgument, ks []KeywordArgument, dicts []interface{}) Arguments {
 	return Arguments{
 		positionals:   ps,

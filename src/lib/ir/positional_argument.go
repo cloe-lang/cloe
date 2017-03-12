e58c3a59a32106222ff883169611ccb86440e4ad
Yota Toyama <raviqqe@gmail.com>
2017-03-12 20:34:57 +0900

    Add documentation on ir/positional_argument.go

--- src/lib/ir/positional_argument.go
+++ src/lib/ir/positional_argument.go
@@ -2,11 +2,14 @@ package ir
 
 import "github.com/raviqqe/tisp/src/lib/core"
 
+// PositionalArgument represents a positional argument passed to a function.
+// It can be a list value and expanded into multiple arguments.
 type PositionalArgument struct {
 	value    interface{}
 	expanded bool
 }
 
+// NewPositionalArgument creates a positional argument.
 func NewPositionalArgument(ir interface{}, expanded bool) PositionalArgument {
 	return PositionalArgument{
 		value:    ir,

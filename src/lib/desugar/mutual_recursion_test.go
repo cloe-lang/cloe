bfc5315315f87048a3b4795cac3f529376096b11
Yota Toyama <raviqqe@gmail.com>
2017-03-17 17:25:31 +0900

    Test desugar.letStatementsToNames()

---
+++ src/lib/desugar/mutual_recursion_test.go
@@ -0,0 +1,36 @@
+package desugar
+
+import (
+	"testing"
+
+	"github.com/raviqqe/tisp/src/lib/ast"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLetStatementsToNames(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		for _, ls := range [][]interface{}{
+			{},
+			{ast.NewLetVar("foo", "bar")},
+			{ast.NewLetVar("foo", "nil"), ast.NewLetVar("bar", "nil")},
+			{ast.NewLetVar("foo", "nil"), ast.NewLetVar("bar", "nil"), ast.NewLetVar("baz", "nil")},
+			{ast.NewLetVar("foo0", "nil"), ast.NewLetVar("foo1", "nil"), ast.NewLetVar("foo3", "nil"),
+				ast.NewLetVar("foo4", "nil"), ast.NewLetVar("foo5", "nil"), ast.NewLetVar("foo6", "nil")},
+		} {
+			for i, s := range letStatementsToNames(ls) {
+				var name string
+
+				switch l := ls[i].(type) {
+				case ast.LetFunction:
+					name = l.Name()
+				case ast.LetVar:
+					name = l.Name()
+				default:
+					panic("Unreachable")
+				}
+
+				assert.Equal(t, name, s)
+			}
+		}
+	}
+}

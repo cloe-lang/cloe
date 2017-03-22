c2d65fbb08e83fbbde0cdb35f945bdf2efe85319
Yota Toyama <raviqqe@gmail.com>
2017-03-22 13:55:39 +0900

    Add comments on core/keyword_argument.go

--- src/lib/core/keyword_argument.go
+++ src/lib/core/keyword_argument.go
@@ -1,10 +1,12 @@
 package core
 
+// KeywordArgument represents a keyword argument passed to a function.
 type KeywordArgument struct {
 	name  string
 	value *Thunk
 }
 
+// NewKeywordArgument creates a KeywordArgument from a bound name and its value.
 func NewKeywordArgument(name string, value *Thunk) KeywordArgument {
 	return KeywordArgument{name: name, value: value}
 }

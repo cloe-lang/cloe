e4618df7771fcd68ef3788bade136096bc91c5b8
Yota Toyama <raviqqe@gmail.com>
2017-03-22 16:00:52 +0900

    Add comments in ast/signature.go

--- src/lib/ast/signature.go
+++ src/lib/ast/signature.go
@@ -2,17 +2,22 @@ package ast
 
 import "fmt"
 
+// Signature represents a signature of a function.
 type Signature struct {
 	positionals halfSignature
 	keywords    halfSignature
 }
 
+// halfSignature represents a half of a signature.
 type halfSignature struct {
 	requireds []string
 	optionals []OptionalArgument
 	rest      string
 }
 
+// NewSignature creates a Signature from {positional, keyword} x
+// {required, optional} arguments and a positional rest argument
+// and a keyword rest argument.
 func NewSignature(
 	pr []string, po []OptionalArgument, pp string,
 	kr []string, ko []OptionalArgument, kk string) Signature {
@@ -22,30 +27,37 @@ func NewSignature(
 	}
 }
 
+// PosReqs returns positional required arguments of a signature.
 func (s Signature) PosReqs() []string {
 	return s.positionals.requireds
 }
 
+// PosOpts returns positional optional arguments of a signature.
 func (s Signature) PosOpts() []OptionalArgument {
 	return s.positionals.optionals
 }
 
+// PosRest returns a positional rest argument of a signature.
 func (s Signature) PosRest() string {
 	return s.positionals.rest
 }
 
+// KeyReqs returns keyword required arguments of a signature.
 func (s Signature) KeyReqs() []string {
 	return s.keywords.requireds
 }
 
+// KeyOpts returns keyword optional arguments of a signature.
 func (s Signature) KeyOpts() []OptionalArgument {
 	return s.keywords.optionals
 }
 
+// KeyRest returns a keyword rest argument of a signature.
 func (s Signature) KeyRest() string {
 	return s.keywords.rest
 }
 
+// Arity returns a number of arguments in a signature.
 func (s Signature) Arity() int {
 	return s.positionals.arity() + s.keywords.arity()
 }
@@ -60,6 +72,7 @@ func (as halfSignature) arity() int {
 	return len(as.requireds) + len(as.optionals) + rest
 }
 
+// NameToIndex converts an argument name into an index in arguments inside a signature.
 func (s Signature) NameToIndex(name string) (int, error) {
 	i, ok := s.positionals.nameToIndex(name)
 

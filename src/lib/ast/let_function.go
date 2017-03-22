ae65681c20e631de901861711f98e958dbc97058
Yota Toyama <raviqqe@gmail.com>
2017-03-22 14:21:13 +0900

    Add comments of ast/let_*.go

--- src/lib/ast/let_function.go
+++ src/lib/ast/let_function.go
@@ -2,6 +2,7 @@ package ast
 
 import "github.com/raviqqe/tisp/src/lib/debug"
 
+// LetFunction represents a let-function statement node in ASTs.
 type LetFunction struct {
 	name      string
 	signature Signature
@@ -10,28 +11,34 @@ type LetFunction struct {
 	info      debug.Info
 }
 
+// NewLetFunction creates a LetFunction from its function name, signature,
+// internal let statements, and a body expression.
 func NewLetFunction(name string, sig Signature, lets []interface{}, expr interface{}, i debug.Info) LetFunction {
 	return LetFunction{name, sig, lets, expr, i}
 }
 
+// Name returns a name of a function defined by the let-function statement.
 func (f LetFunction) Name() string {
 	return f.name
 }
 
+// Signature returns a signature of a function defined by the let-function statement.
 func (f LetFunction) Signature() Signature {
 	return f.signature
 }
 
-// Lets returns let statements contained in this let-function statement.
-// Return values should be LetVar or LetFunction.
+// Lets returns let statements contained in the let-function statement.
+// Returned values should be LetVar or LetFunction.
 func (f LetFunction) Lets() []interface{} {
 	return f.lets
 }
 
+// Body returns a body expression of a function defined by the let-function statement.
 func (f LetFunction) Body() interface{} {
 	return f.body
 }
 
+// DebugInfo returns debug information of a function defined by the let-function statement.
 func (f LetFunction) DebugInfo() debug.Info {
 	return f.info
 }
--- src/lib/ast/let_var.go
+++ src/lib/ast/let_var.go
@@ -1,18 +1,22 @@
 package ast
 
+// LetVar represents a let-variable statement node in ASTs.
 type LetVar struct {
 	name string
 	expr interface{}
 }
 
+// NewLetVar creates a LetVar from a variable name and its value of an expression.
 func NewLetVar(name string, expr interface{}) LetVar {
 	return LetVar{name, expr}
 }
 
+// Name returns a variable name defined by the let-variable statement.
 func (c LetVar) Name() string {
 	return c.name
 }
 
+// Expr returns an expression of a variable value""
 func (c LetVar) Expr() interface{} {
 	return c.expr
 }

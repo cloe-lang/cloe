e9d085c02b5446e2a8e227bf87efb2f9c07c11cc
Yota Toyama <raviqqe@gmail.com>
2017-05-27 02:04:11 +0900

    Improve description of OutputType

--- src/lib/core/output.go
+++ src/lib/core/output.go
@@ -1,6 +1,10 @@
 package core
 
 // OutputType represents an output value returned from an impure function.
+// OutputType is meant to be used to distinguish calls of pure and impure
+// functions and also represent a "result" value of an impure function which
+// can be extracted by a special function named "out" and passed to a pure
+// function.
 type OutputType struct {
 	value *Thunk
 }

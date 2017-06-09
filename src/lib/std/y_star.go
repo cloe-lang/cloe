79004ce0f44de0bd46ffdf2b84fefc07ffc0cc37
Yota Toyama <raviqqe@gmail.com>
2017-06-10 02:48:58 +0900

    Split y.go

---
+++ src/lib/std/y_star.go
@@ -0,0 +1,72 @@
+package std
+
+import "github.com/tisp-lang/tisp/src/lib/core"
+
+// Ys is Y* combinator which takes functions whose first arguments are a list
+// of themselves applied to the combinator.
+var Ys = core.NewLazyFunction(
+	core.NewSignature(
+		nil, nil, "functions",
+		nil, nil, "",
+	),
+	func(ts ...*core.Thunk) core.Value {
+		v := ts[0].Eval()
+		l, ok := v.(core.ListType)
+
+		if !ok {
+			panic("Rest arguments must be a list.")
+		}
+
+		fs, err := l.ToThunks()
+
+		if err != nil {
+			return err
+		}
+
+		if len(fs) == 0 {
+			return core.NumArgsError("ys", ">= 1")
+		}
+
+		f := core.NewLazyFunction(
+			core.NewSignature(
+				[]string{"x"}, nil, "",
+				nil, nil, "",
+			),
+			func(ps ...*core.Thunk) core.Value {
+				p := ps[0]
+
+				applyF := core.NewLazyFunction(
+					core.NewSignature(
+						[]string{"f"}, nil, "args",
+						nil, nil, "kwargs",
+					),
+					func(qs ...*core.Thunk) core.Value {
+						return core.App(qs[0], core.NewArguments(
+							[]core.PositionalArgument{
+								core.NewPositionalArgument(core.PApp(p, p), false),
+								core.NewPositionalArgument(qs[1], true),
+							},
+							nil,
+							[]*core.Thunk{qs[2]}))
+					})
+
+				newFs := make([]*core.Thunk, len(fs))
+
+				for i, f := range fs {
+					newFs[i] = core.PApp(core.Partial, applyF, f)
+				}
+
+				return core.NewList(newFs...)
+			})
+
+		return core.PApp(xx, f)
+	})
+
+var xx = core.NewLazyFunction(
+	core.NewSignature(
+		[]string{"x"}, nil, "",
+		nil, nil, "",
+	),
+	func(ts ...*core.Thunk) core.Value {
+		return core.PApp(ts[0], ts[0])
+	})
---
+++ src/lib/std/y_star_test.go
@@ -0,0 +1,51 @@
+package std
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tisp-lang/tisp/src/lib/core"
+)
+
+func TestYsMultipleFs(t *testing.T) {
+	evenWithExtraArg := core.NewLazyFunction(
+		core.NewSignature(
+			[]string{"fs", "dummyArg", "num"}, nil, "",
+			nil, nil, "",
+		),
+		func(ts ...*core.Thunk) core.Value {
+			n := ts[2]
+
+			return core.PApp(core.If,
+				core.PApp(core.Equal, n, core.NewNumber(0)),
+				core.True,
+				core.PApp(core.PApp(core.First, core.PApp(core.Rest, ts[0])), core.PApp(core.Sub, n, core.NewNumber(1))))
+		})
+
+	odd := core.NewLazyFunction(
+		core.NewSignature(
+			[]string{"fs", "num"}, nil, "",
+			nil, nil, "",
+		),
+		func(ts ...*core.Thunk) core.Value {
+			n := ts[1]
+
+			return core.PApp(core.If,
+				core.PApp(core.Equal, n, core.NewNumber(0)),
+				core.False,
+				core.PApp(core.PApp(core.First, ts[0]), core.Nil, core.PApp(core.Sub, n, core.NewNumber(1))))
+		})
+
+	fs := core.PApp(Ys, evenWithExtraArg, odd)
+
+	for _, n := range []float64{0, 1, 2, 3, 4, 5, 6, 42, 100, 121, 256, 1023} {
+		b1 := bool(core.PApp(core.PApp(core.First, fs), core.NewString("unused"), core.NewNumber(n)).Eval().(core.BoolType))
+		b2 := bool(core.PApp(core.PApp(core.First, core.PApp(core.Rest, fs)), core.NewNumber(n)).Eval().(core.BoolType))
+
+		t.Logf("n = %v, even? %v, odd? %v\n", n, b1, b2)
+
+		rem := int(n) % 2
+		assert.Equal(t, b1, rem == 0)
+		assert.Equal(t, b2, rem != 0)
+	}
+}

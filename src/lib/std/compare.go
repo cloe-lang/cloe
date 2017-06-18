c0a39f630428026a36641c23932db663d69762b7
Yota Toyama <raviqqe@gmail.com>
2017-06-19 02:14:38 +0900

    Implement less-or-equal, greater, and greater-or-equal functions

--- src/lib/std/compare.go
+++ src/lib/std/compare.go
@@ -2,42 +2,49 @@ package std
 
 import "github.com/tisp-lang/tisp/src/lib/core"
 
-// TODO: Implement LessEq, Greater, and GreaterEq functions.
-
-func compare(checkOrder func(core.NumberType) bool) func(ts ...*core.Thunk) core.Value {
-	return func(ts ...*core.Thunk) core.Value {
-		l := ts[0]
-
-		if v := checkEmptyList(l, core.True); v != nil {
-			return v
-		}
-
-		prev := core.PApp(core.First, l)
-
-		for {
-			l = core.PApp(core.Rest, l)
+func createCompareFunction(checkOrder func(core.NumberType) bool) *core.Thunk {
+	return core.NewLazyFunction(
+		core.NewSignature([]string{}, nil, "xs", nil, nil, ""),
+		func(ts ...*core.Thunk) core.Value {
+			l := ts[0]
 
 			if v := checkEmptyList(l, core.True); v != nil {
 				return v
 			}
 
-			current := core.PApp(core.First, l)
+			prev := core.PApp(core.First, l)
 
-			v := core.PApp(core.Compare, prev, current).Eval()
-			n, ok := v.(core.NumberType)
+			for {
+				l = core.PApp(core.Rest, l)
 
-			if !ok {
-				return core.NotBoolError(v)
-			} else if !checkOrder(n) {
-				return core.False
+				if v := checkEmptyList(l, core.True); v != nil {
+					return v
+				}
+
+				current := core.PApp(core.First, l)
+
+				v := core.PApp(core.Compare, prev, current).Eval()
+				n, ok := v.(core.NumberType)
+
+				if !ok {
+					return core.NotBoolError(v)
+				} else if !checkOrder(n) {
+					return core.False
+				}
+
+				prev = current
 			}
-
-			prev = current
-		}
-	}
+		})
 }
 
 // Less checks if arguments are aligned in ascending order or not.
-var Less = core.NewLazyFunction(
-	core.NewSignature([]string{}, nil, "xs", nil, nil, ""),
-	compare(func(n core.NumberType) bool { return n == -1 }))
+var Less = createCompareFunction(func(n core.NumberType) bool { return n == -1 })
+
+// LessEq checks if arguments are aligned in ascending order or not.
+var LessEq = createCompareFunction(func(n core.NumberType) bool { return n == -1 || n == 0 })
+
+// Greater checks if arguments are aligned in ascending order or not.
+var Greater = createCompareFunction(func(n core.NumberType) bool { return n == 1 })
+
+// GreaterEq checks if arguments are aligned in ascending order or not.
+var GreaterEq = createCompareFunction(func(n core.NumberType) bool { return n == 1 || n == 0 })

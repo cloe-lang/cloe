a298a8ca3d73d6b5917848c185822a114ef135de
Yota Toyama <raviqqe@gmail.com>
2017-06-19 02:21:11 +0900

    Test less-or-equal function

--- src/lib/std/compare_test.go
+++ src/lib/std/compare_test.go
@@ -32,6 +32,7 @@ func TestLessFalse(t *testing.T) {
 		{core.NewNumber(42), core.NewNumber(0)},
 		{core.NewNumber(0), core.NewNumber(42), core.NewNumber(42)},
 		{core.NewNumber(0), core.NewNumber(0), core.NewNumber(42)},
+		{core.NewString("bar"), core.NewString("bar"), core.NewString("baz")},
 		{core.NewString("foo"), core.NewString("bar"), core.NewString("baz")},
 		{core.NewList(core.NewList(core.NewNumber(42))),
 			core.NewList(core.NewList(core.NewNumber(42), core.NewNumber(42))),
@@ -41,5 +42,43 @@ func TestLessFalse(t *testing.T) {
 			core.NewList(core.NewList(core.NewNumber(42), core.NewNumber(2049)))},
 	} {
 		assert.True(t, !bool(core.PApp(Equal, ts...).Eval().(core.BoolType)))
+	}
+}
+
+func TestLessEqTrue(t *testing.T) {
+	for _, ts := range [][]*core.Thunk{
+		{},
+		{core.NewNumber(42)},
+		{core.NewString("foo")},
+		{core.NewList(core.NewNumber(42))},
+		{core.NewNumber(0), core.NewNumber(0.0000000000001)},
+		{core.NewNumber(0), core.NewNumber(0.00000001), core.NewNumber(0.0001)},
+		{core.NewString("bar"), core.NewString("baz"), core.NewString("foo")},
+		{core.NewList(core.NewNumber(42)), core.NewList(core.NewNumber(2049))},
+		{core.NewList(core.NewNumber(42)), core.NewList(core.NewNumber(42), core.NewNumber(42))},
+		{core.NewList(core.NewList(core.NewNumber(42))),
+			core.NewList(core.NewList(core.NewNumber(42), core.NewNumber(42))),
+			core.NewList(core.NewList(core.NewNumber(42), core.NewNumber(42), core.NewNumber(42)))},
+
+		{core.NewNumber(0), core.NewNumber(42), core.NewNumber(42)},
+		{core.NewNumber(0), core.NewNumber(0), core.NewNumber(42)},
+		{core.NewString("bar"), core.NewString("bar"), core.NewString("baz")},
+		{core.NewList(core.NewList(core.NewNumber(42))),
+			core.NewList(core.NewList(core.NewNumber(42), core.NewNumber(42))),
+			core.NewList(core.NewList(core.NewNumber(42), core.NewNumber(42)))},
+		{core.NewList(core.NewList(core.NewNumber(42), core.NewNumber(42))),
+			core.NewList(core.NewList(core.NewNumber(42), core.NewNumber(42))),
+			core.NewList(core.NewList(core.NewNumber(42), core.NewNumber(2049)))},
+	} {
+		assert.True(t, bool(core.PApp(LessEq, ts...).Eval().(core.BoolType)))
+	}
+}
+
+func TestLessEqFalse(t *testing.T) {
+	for _, ts := range [][]*core.Thunk{
+		{core.NewNumber(42), core.NewNumber(0)},
+		{core.NewString("foo"), core.NewString("bar"), core.NewString("baz")},
+	} {
+		assert.True(t, !bool(core.PApp(LessEq, ts...).Eval().(core.BoolType)))
 	}
 }

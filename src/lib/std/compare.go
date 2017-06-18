254b55db9fac94ad3b464a8e512e8ad101f6c614
Yota Toyama <raviqqe@gmail.com>
2017-06-19 01:33:12 +0900

    Rename std/compare.go as std/equal.go

--- src/lib/std/compare.go
+++ src/lib/std/compare.go
@@ -1,39 +1,3 @@
 package std
 
-import "github.com/tisp-lang/tisp/src/lib/core"
-
-// Equal checks if all arguments are equal or not, and returns true if so or false otherwise.
-var Equal = core.NewStrictFunction(
-	core.NewSignature(
-		[]string{}, nil, "xs",
-		nil, nil, "",
-	),
-	func(ts ...*core.Thunk) (v core.Value) {
-		l := ts[0]
-
-		if v := checkEmptyList(l, core.True); v != nil {
-			return v
-		}
-
-		e0 := core.PApp(core.First, l)
-		l = core.PApp(core.Rest, l)
-
-		for {
-			if v := checkEmptyList(l, core.True); v != nil {
-				return v
-			}
-
-			v := core.PApp(core.Equal, e0, core.PApp(core.First, l)).Eval()
-			b, ok := v.(core.BoolType)
-
-			if !ok {
-				return core.NotBoolError(v)
-			} else if !b {
-				return core.False
-			}
-
-			l = core.PApp(core.Rest, l)
-		}
-	})
-
 // TODO: Implement Less, LessEq, Greater, and GreaterEq functions.

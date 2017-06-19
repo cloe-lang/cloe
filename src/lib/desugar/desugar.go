da95c84146a91da7d89932cedc384712a8e087c3
Yota Toyama <raviqqe@gmail.com>
2017-06-20 03:00:15 +0900

    Make Desugar function desugar a statement

--- src/lib/desugar/desugar.go
+++ src/lib/desugar/desugar.go
@@ -1,14 +1,12 @@
 package desugar
 
-// Desugar desugars a module of statements in AST.
-func Desugar(module []interface{}) []interface{} {
+// Desugar desugars a statement in AST.
+func Desugar(s interface{}) []interface{} {
 	new := make([]interface{}, 0)
 
-	for _, s := range module {
-		for _, s := range desugarMutualRecursionStatement(s) {
-			for _, s := range desugarSelfRecursiveStatement(s) {
-				new = append(new, flattenStatement(s)...)
-			}
+	for _, s := range desugarMutualRecursionStatement(s) {
+		for _, s := range desugarSelfRecursiveStatement(s) {
+			new = append(new, flattenStatement(s)...)
 		}
 	}
 

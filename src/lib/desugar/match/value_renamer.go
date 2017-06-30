d6a02bd5ad149b61568d6bf0ba385c43d5d9c4bf
Yota Toyama <raviqqe@gmail.com>
2017-07-01 01:59:50 +0900

    Implement value renamer

--- src/lib/desugar/match/value_renamer.go
+++ src/lib/desugar/match/value_renamer.go
@@ -1,5 +1,11 @@
 package match
 
+import (
+	"fmt"
+
+	"github.com/tisp-lang/tisp/src/lib/ast"
+)
+
 type valueRenamer struct {
 	nameMap map[string]string
 }
@@ -8,6 +14,41 @@ func newValueRenamer(m map[string]string) valueRenamer {
 	return valueRenamer{m}
 }
 
-func (r valueRenamer) rename(x interface{}) interface{} {
-	panic("Not implemented")
+func (r valueRenamer) rename(v interface{}) interface{} {
+	switch x := v.(type) {
+	case string:
+		if n, ok := r.nameMap[x]; ok {
+			return n
+		}
+
+		return x
+	case ast.App:
+		ps := x.Arguments().Positionals()
+		newPs := make([]ast.PositionalArgument, 0, len(ps))
+
+		for _, p := range ps {
+			newPs = append(newPs, ast.NewPositionalArgument(r.rename(p.Value()), p.Expanded()))
+		}
+
+		ks := x.Arguments().Keywords()
+		newKs := make([]ast.KeywordArgument, 0, len(ks))
+
+		for _, k := range ks {
+			newKs = append(newKs, ast.NewKeywordArgument(k.Name(), r.rename(k.Value())))
+		}
+
+		ds := x.Arguments().ExpandedDicts()
+		newDs := make([]interface{}, 0, len(ds))
+
+		for _, d := range ds {
+			newDs = append(newDs, r.rename(d))
+		}
+
+		return ast.NewApp(
+			r.rename(x.Function()),
+			ast.NewArguments(newPs, newKs, newDs),
+			x.DebugInfo())
+	}
+
+	panic(fmt.Errorf("Invalid pattern: %#v", v))
 }

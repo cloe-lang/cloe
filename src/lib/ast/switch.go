b7d4f1754b253a5b0e4944c459b6e26a7e4515ae
Yota Toyama <raviqqe@gmail.com>
2017-07-08 02:43:36 +0900

    Add default case to switch expression of AST

--- src/lib/ast/switch.go
+++ src/lib/ast/switch.go
@@ -2,17 +2,18 @@ package ast
 
 // Switch represents a switch expression.
 type Switch struct {
-	value interface{}
-	cases []SwitchCase
+	value       interface{}
+	cases       []SwitchCase
+	defaultCase interface{}
 }
 
 // NewSwitch creates a match expression.
-func NewSwitch(v interface{}, cs []SwitchCase) Switch {
+func NewSwitch(v interface{}, cs []SwitchCase, d interface{}) Switch {
 	if len(cs) == 0 {
 		panic("Cases in a match expression must be more than 0.")
 	}
 
-	return Switch{v, cs}
+	return Switch{v, cs, d}
 }
 
 // Value returns a value which will be matched with patterns in a match expression.
@@ -23,4 +24,9 @@ func (s Switch) Value() interface{} {
 // Cases returns pairs of a pattern and corrensponding value in a match expression.
 func (s Switch) Cases() []SwitchCase {
 	return s.cases
+}
+
+// DefaultCase returns a default case in a switch expression.
+func (s Switch) DefaultCase() interface{} {
+	return s.defaultCase
 }

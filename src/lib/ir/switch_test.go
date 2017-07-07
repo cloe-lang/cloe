8c4d260e7f5ec71bb2ccf787dedc49b30b700261
Yota Toyama <raviqqe@gmail.com>
2017-07-08 02:14:09 +0900

    Test switch expression in a function

--- src/lib/ir/switch_test.go
+++ src/lib/ir/switch_test.go
@@ -3,6 +3,7 @@ package ir
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/tisp-lang/tisp/src/lib/core"
 )
 
@@ -22,4 +23,17 @@ func TestNewSwitchNoPattern(t *testing.T) {
 
 func TestSwitchCompileToDict(t *testing.T) {
 	NewSwitch(0, []Case{NewCase(core.Nil, 1)}).compileToDict()
+}
+
+func TestSwitchInFunction(t *testing.T) {
+	f := CompileFunction(
+		core.NewSignature([]string{"x"}, nil, "", nil, nil, ""),
+		nil,
+		NewSwitch(0, []Case{
+			NewCase(core.NewString("foo"), core.NewNumber(42)),
+			NewCase(core.True, core.NewNumber(2049)),
+		}))
+
+	assert.Equal(t, 42.0, float64(core.PApp(f, core.NewString("foo")).Eval().(core.NumberType)))
+	assert.Equal(t, 2049.0, float64(core.PApp(f, core.True).Eval().(core.NumberType)))
 }

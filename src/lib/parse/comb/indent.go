4659d2d6d8320cf5787552148d4a469e161cd246
Yota Toyama <raviqqe@gmail.com>
2017-07-14 02:41:54 +0900

    Add SameLineOrIndented combinator

--- src/lib/parse/comb/indent.go
+++ src/lib/parse/comb/indent.go
@@ -47,6 +47,20 @@ func (s *State) SameLine(p, q Parser) Parser {
 	})
 }
 
+// SameLineOrIndented creates a parser whiich parses something in the same line
+// or indented. It retrieves a reference position saved by WithPosition
+// combinator implicitly differently from other indent-aware combinators.
+func (s *State) SameLineOrIndented(n int, p Parser) Parser {
+	return func() (interface{}, error) {
+		if s.current.lineNumber != s.reference.lineNumber &&
+			s.current.linePosition < s.reference.linePosition+n {
+			return nil, fmt.Errorf("Invalid indent")
+		}
+
+		return p()
+	}
+}
+
 // WithPosition saves a current position and runs a given parser.
 func (s *State) WithPosition(p Parser) Parser {
 	return func() (interface{}, error) {
--- src/lib/parse/comb/indent_test.go
+++ src/lib/parse/comb/indent_test.go
@@ -79,3 +79,47 @@ func TestSameLineFail(t *testing.T) {
 		assert.NotEqual(t, err, nil)
 	}
 }
+
+func TestSameLineOrIndented(t *testing.T) {
+	for _, str := range []string{
+		"foo foo",
+		"foo\n  foo",
+		"foo\n   foo",
+		"foo\n\n  foo",
+		"  foo    foo",
+		"  foo\n    foo",
+		"  foo\n     foo",
+		"  foo\n\n    foo",
+	} {
+		s := NewState(str)
+		blank := s.Many(s.InString(" \n"))
+		foo := s.And(s.String("foo"), blank)
+		result, err := s.Exhaust(s.And(blank, s.WithPosition(s.And(foo, s.SameLineOrIndented(2, foo)))))()
+
+		t.Logf("%#v", result)
+
+		assert.NotEqual(t, result, nil)
+		assert.Equal(t, err, nil)
+	}
+}
+
+func TestSameLineOrIndentedFail(t *testing.T) {
+	for _, str := range []string{
+		"foo\nfoo",
+		"foo\n foo",
+		"  foo\nfoo",
+		"  foo\n foo",
+		"  foo\n  foo",
+		"  foo\n   foo",
+	} {
+		s := NewState(str)
+		blank := s.Many(s.InString(" \n"))
+		foo := s.And(s.String("foo"), blank)
+		result, err := s.Exhaust(s.And(blank, s.WithPosition(s.And(foo, s.SameLineOrIndented(2, foo)))))()
+
+		t.Logf("%#v", err)
+
+		assert.Equal(t, result, nil)
+		assert.NotEqual(t, err, nil)
+	}
+}

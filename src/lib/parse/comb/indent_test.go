1629d0dd772ff768bdf7133579f5e1d2069bcd9a
Yota Toyama <raviqqe@gmail.com>
2017-07-14 01:35:50 +0900

    Test failures of block parser

--- src/lib/parse/comb/indent_test.go
+++ src/lib/parse/comb/indent_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBlockWithNoIndent(t *testing.T) {
+func TestBlock(t *testing.T) {
 	for _, test := range []struct {
 		indent int
 		source string
@@ -23,5 +23,25 @@ func TestBlockWithNoIndent(t *testing.T) {
 
 		assert.NotEqual(t, result, nil)
 		assert.Equal(t, err, nil)
+	}
+}
+
+func TestBlockFail(t *testing.T) {
+	for _, test := range []struct {
+		indent int
+		source string
+	}{
+		{0, "foo\n foo\nfoo"},
+		{1, " foo\nfoo\n foo"},
+		{2, "  foo\n  foo\n foo"},
+	} {
+		s := NewState(test.source)
+		blank := s.Many(s.InString(" \n"))
+		result, err := s.Exhaust(s.Block(test.indent, blank, s.And(s.String("foo"), blank)))()
+
+		t.Logf("%#v", err)
+
+		assert.Equal(t, result, nil)
+		assert.NotEqual(t, err, nil)
 	}
 }

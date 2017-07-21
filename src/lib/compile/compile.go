719aaccd3f38c9eadf5a1e462c3fe5eecf8a869c
Yota Toyama <raviqqe@gmail.com>
2017-07-22 02:54:19 +0900

    Move readFileOrStdin function

--- src/lib/compile/compile.go
+++ src/lib/compile/compile.go
@@ -1,6 +1,9 @@
 package compile
 
 import (
+	"io/ioutil"
+	"os"
+
 	"github.com/tisp-lang/tisp/src/lib/core"
 	"github.com/tisp-lang/tisp/src/lib/desugar"
 	"github.com/tisp-lang/tisp/src/lib/parse"
@@ -9,7 +12,7 @@ import (
 
 // MainModule compiles a main module of a path into outputs of thunks.
 func MainModule(path string) []Output {
-	module, err := parse.MainModule(util.ReadFileOrStdin(path))
+	module, err := parse.MainModule(readFileOrStdin(path))
 
 	if err != nil {
 		util.PanicError(err)
@@ -21,7 +24,7 @@ func MainModule(path string) []Output {
 
 // SubModule compiles a sub module of a path into a map of names to thunks.
 func SubModule(path string) map[string]*core.Thunk {
-	return subModule(util.ReadFileOrStdin(path))
+	return subModule(readFileOrStdin(path))
 }
 
 func subModule(filename, source string) map[string]*core.Thunk {
@@ -34,4 +37,27 @@ func subModule(filename, source string) map[string]*core.Thunk {
 	c := newCompiler()
 	c.compile(desugar.Desugar(module))
 	return c.env.toMap()
+}
+
+func readFileOrStdin(filename string) (string, string) {
+	file := os.Stdin
+
+	if filename == "" {
+		filename = "<stdin>"
+	} else {
+		var err error
+		file, err = os.Open(filename)
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	source, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return filename, string(source)
 }

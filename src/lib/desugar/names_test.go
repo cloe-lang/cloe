912e0b466c04e4ff48bca46f454a75192431fd7e
Yota Toyama <raviqqe@gmail.com>
2017-07-25 02:29:53 +0900

    Test names with let-function nodes

--- src/lib/desugar/names_test.go
+++ src/lib/desugar/names_test.go
@@ -5,9 +5,49 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/tisp-lang/tisp/src/lib/ast"
+	"github.com/tisp-lang/tisp/src/lib/debug"
 )
 
-func TestNamesFind(t *testing.T) {
+func TestNamesFindWithLetVar(t *testing.T) {
 	n := "x"
 	assert.True(t, newNames(n).find(ast.NewLetVar(n, n)).include(n))
+}
+
+func TestNamesFindWithLetFunction(t *testing.T) {
+	n := "x"
+
+	for _, test := range []struct {
+		letFunc ast.LetFunction
+		answer  bool
+	}{
+		{
+			ast.NewLetFunction(
+				n,
+				ast.NewSignature(nil, nil, "", nil, nil, ""),
+				nil,
+				n,
+				debug.NewGoInfo(0)),
+			true,
+		},
+		{
+			ast.NewLetFunction(
+				n,
+				ast.NewSignature(nil, nil, "", nil, nil, ""),
+				[]interface{}{ast.NewLetVar(n, n)},
+				n,
+				debug.NewGoInfo(0)),
+			true,
+		},
+		{
+			ast.NewLetFunction(
+				n,
+				ast.NewSignature(nil, nil, "", nil, nil, ""),
+				[]interface{}{ast.NewLetVar(n, "y")},
+				n,
+				debug.NewGoInfo(0)),
+			false,
+		},
+	} {
+		assert.Equal(t, test.answer, newNames(n).find(test.letFunc).include(n))
+	}
 }

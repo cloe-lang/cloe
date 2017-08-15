39360dfa27c96995f434c290876ae19149cf8ac1
Yota Toyama <raviqqe@gmail.com>
2017-08-15 22:33:57 +0900

    Desugar inner let-variable statements

--- src/lib/desugar/recursive_let_var.go
+++ src/lib/desugar/recursive_let_var.go
@@ -7,21 +7,43 @@ import (
 )
 
 func desugarRecursiveLetVar(s interface{}) []interface{} {
-	l, ok := s.(ast.LetVar)
+	switch s := s.(type) {
+	case ast.LetVar:
+		if len(newNames(s.Name()).find(s.Expr())) == 0 {
+			return []interface{}{s}
+		}
 
-	if !ok || len(newNames(l.Name()).find(l.Expr())) == 0 {
-		return []interface{}{s}
+		n := s.Name()
+		f := gensym.GenSym()
+
+		return []interface{}{
+			ast.NewLetFunction(
+				n,
+				ast.NewSignature(nil, nil, "", nil, nil, ""),
+				nil,
+				s.Expr(),
+				debug.NewGoInfo(0)),
+			ast.NewLetVar(f, n),
+			ast.NewLetVar(n, ast.NewPApp(f, nil, debug.NewGoInfo(0)))}
+	case ast.LetFunction:
+		ls := make([]interface{}, 0, 3*len(s.Lets()))
+
+		for _, l := range s.Lets() {
+			ls = append(ls, desugarRecursiveLetVar(l)...)
+		}
+
+		return []interface{}{ast.NewLetFunction(s.Name(), s.Signature(), ls, s.Body(), s.DebugInfo())}
+	case ast.MutualRecursion:
+		fs := make([]ast.LetFunction, 0, len(s.LetFunctions()))
+
+		for _, f := range s.LetFunctions() {
+			for _, l := range desugarRecursiveLetVar(f) {
+				fs = append(fs, l.(ast.LetFunction))
+			}
+		}
+
+		return []interface{}{ast.NewMutualRecursion(fs, s.DebugInfo())}
 	}
 
-	f := gensym.GenSym()
-
-	return []interface{}{
-		ast.NewLetFunction(
-			l.Name(),
-			ast.NewSignature(nil, nil, "", nil, nil, ""),
-			nil,
-			l.Expr(),
-			debug.NewGoInfo(0)),
-		ast.NewLetVar(f, l.Name()),
-		ast.NewLetVar(l.Name(), ast.NewPApp(f, nil, debug.NewGoInfo(0)))}
+	return []interface{}{s}
 }

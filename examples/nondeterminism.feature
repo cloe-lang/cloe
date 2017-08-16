593e4b8a92c4475c8bc1e15b0aa2453e5958bdca
Yota Toyama <raviqqe@gmail.com>
2017-08-17 00:36:40 +0900

    Fix recursive let-variable statements

--- examples/nondeterminism.feature
+++ examples/nondeterminism.feature
@@ -2,12 +2,12 @@ Feature: Nondeterminism
   Scenario: Apply rally function to a infinite list
     Given a file named "main.tisp" with:
     """
-    (let many42 (prepend 42 many42))
-    (write (first many42))
-    (let many42 (rest many42))
-    (write (first many42))
-    (let many42 (rest many42))
-    (write (first many42))
+    (let a (prepend 42 a))
+    (write (first a))
+    (let b (rest a))
+    (write (first b))
+    (let c (rest b))
+    (write (first c))
     """
     When I successfully run `tisp main.tisp`
     Then the stdout should contain exactly:
--- src/lib/desugar/recursive_let_var.go
+++ src/lib/desugar/recursive_let_var.go
@@ -6,14 +6,24 @@ import (
 	"github.com/tisp-lang/tisp/src/lib/gensym"
 )
 
-func desugarRecursiveLetVar(s interface{}) []interface{} {
-	switch s := s.(type) {
+func desugarRecursiveLetVar(x interface{}) []interface{} {
+	y := ast.Convert(convertRecursiveLetVar, x)
+
+	if ys, ok := y.([]interface{}); ok {
+		return ys
+	}
+
+	return []interface{}{y}
+}
+
+func convertRecursiveLetVar(x interface{}) interface{} {
+	switch x := x.(type) {
 	case ast.LetVar:
-		if len(newNames(s.Name()).find(s.Expr())) == 0 {
-			return []interface{}{s}
+		if len(newNames(x.Name()).find(x.Expr())) == 0 {
+			return x
 		}
 
-		n := s.Name()
+		n := x.Name()
 		f := gensym.GenSym()
 
 		return []interface{}{
@@ -21,29 +31,25 @@ func desugarRecursiveLetVar(s interface{}) []interface{} {
 				n,
 				ast.NewSignature(nil, nil, "", nil, nil, ""),
 				nil,
-				s.Expr(),
+				ast.Convert(func(x interface{}) interface{} {
+					if m, ok := x.(string); ok && n == m {
+						return ast.NewPApp(n, nil, debug.NewGoInfo(0))
+					}
+
+					return nil
+				}, x.Expr()),
 				debug.NewGoInfo(0)),
 			ast.NewLetVar(f, n),
 			ast.NewLetVar(n, ast.NewPApp(f, nil, debug.NewGoInfo(0)))}
 	case ast.LetFunction:
-		ls := make([]interface{}, 0, 3*len(s.Lets()))
+		ls := make([]interface{}, 0, 3*len(x.Lets()))
 
-		for _, l := range s.Lets() {
+		for _, l := range x.Lets() {
 			ls = append(ls, desugarRecursiveLetVar(l)...)
 		}
 
-		return []interface{}{ast.NewLetFunction(s.Name(), s.Signature(), ls, s.Body(), s.DebugInfo())}
-	case ast.MutualRecursion:
-		fs := make([]ast.LetFunction, 0, len(s.LetFunctions()))
-
-		for _, f := range s.LetFunctions() {
-			for _, l := range desugarRecursiveLetVar(f) {
-				fs = append(fs, l.(ast.LetFunction))
-			}
-		}
-
-		return []interface{}{ast.NewMutualRecursion(fs, s.DebugInfo())}
+		return ast.NewLetFunction(x.Name(), x.Signature(), ls, x.Body(), x.DebugInfo())
 	}
 
-	return []interface{}{s}
+	return nil
 }

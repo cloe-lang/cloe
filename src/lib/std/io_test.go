2806eb2d6173783ab2b5ea36953584e46a6b2656
Yota Toyama <raviqqe@gmail.com>
2017-08-18 23:21:24 +0900

    Test write function

--- src/lib/std/io_test.go
+++ src/lib/std/io_test.go
@@ -10,3 +10,30 @@ import (
 func TestReadWithStdin(t *testing.T) {
 	assert.Equal(t, core.StringType(""), core.PApp(Read).Eval())
 }
+
+func TestWrite(t *testing.T) {
+	for _, a := range []core.Arguments{
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
+			nil,
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
+			[]core.KeywordArgument{core.NewKeywordArgument("sep", core.NewString(","))},
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
+			[]core.KeywordArgument{core.NewKeywordArgument("end", core.NewString(""))},
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
+			[]core.KeywordArgument{core.NewKeywordArgument("file", core.NewString("/tmp/tisp.test"))},
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
+			[]core.KeywordArgument{core.NewKeywordArgument("mode", core.NewNumber(0775))},
+			nil),
+	} {
+		assert.Equal(t, core.Nil.Eval(), core.App(Write, a).EvalOutput())
+	}
+}

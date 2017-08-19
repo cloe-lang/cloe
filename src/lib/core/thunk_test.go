793f582fce04807a066e0ecf49fa071a6cc2db16
Yota Toyama <raviqqe@gmail.com>
2017-08-19 22:32:04 +0900

    Test Thunk.EvalOutput()

--- src/lib/core/thunk_test.go
+++ src/lib/core/thunk_test.go
@@ -6,21 +6,41 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestThunkEvalFailByCallingError(t *testing.T) {
+func TestThunkEvalByCallingError(t *testing.T) {
 	e := PApp(NewError("Apple", "pen.")).Eval().(ErrorType)
 	t.Log(e)
 	assert.Equal(t, 1, len(e.callTrace))
 }
 
-func TestThunkEvalFailByCallingErrorTwice(t *testing.T) {
+func TestThunkEvalByCallingErrorTwice(t *testing.T) {
 	e := PApp(PApp(NewError("Apple", "pen."))).Eval().(ErrorType)
 	t.Log(e)
 	assert.Equal(t, 2, len(e.callTrace))
 }
 
-func TestThunkEvalOutputFail(t *testing.T) {
+func TestThunkEvalOutput(t *testing.T) {
+	s := NewString("foo")
+	assert.Equal(t,
+		s.Eval(),
+		PApp(NewLazyFunction(
+			NewSignature([]string{"x"}, nil, "", nil, nil, ""),
+			func(ts ...*Thunk) Value {
+				return NewOutput(ts[0])
+			}), s).EvalOutput())
+}
+
+func TestThunkEvalOutputWithNonOutput(t *testing.T) {
 	v := Nil.EvalOutput()
-	_, ok := v.(ErrorType)
+	err, ok := v.(ErrorType)
 	t.Logf("%#v\n", v)
 	assert.True(t, ok)
+	assert.Equal(t, err.name, "TypeError")
+}
+
+func TestThunkEvalOutputWithError(t *testing.T) {
+	v := OutOfRangeError().EvalOutput()
+	err, ok := v.(ErrorType)
+	t.Logf("%#v\n", v)
+	assert.True(t, ok)
+	assert.Equal(t, err.name, "OutOfRangeError")
 }

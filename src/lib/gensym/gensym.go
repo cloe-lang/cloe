214c2b6bb1950d98ab91fe07e0e23fbb44d71523
Yota Toyama <raviqqe@gmail.com>
2017-12-07 01:43:00 +0900

    Generate simpler symbols of $ + number

--- src/lib/gensym/gensym.go
+++ src/lib/gensym/gensym.go
@@ -9,5 +9,5 @@ var index uint64
 
 // GenSym creates a unique symbol as a string from its components.
 func GenSym() string {
-	return "$gensym$" + fmt.Sprint(atomic.AddUint64(&index, 1)-1)
+	return "$" + fmt.Sprint(atomic.AddUint64(&index, 1)-1)
 }

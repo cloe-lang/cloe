0aa644053150162d5d94b7d26d05258a3ba67256
Yota Toyama <raviqqe@gmail.com>
2017-12-11 11:23:50 +0900

    Test desugarEmptyCollection function

---
+++ src/lib/desugar/empty_collections_test.go
@@ -0,0 +1,42 @@
+package desugar
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tisp-lang/tisp/src/lib/ast"
+	"github.com/tisp-lang/tisp/src/lib/consts"
+	"github.com/tisp-lang/tisp/src/lib/parse"
+)
+
+func TestDesugarEmptyCollection(t *testing.T) {
+	for _, s := range []string{
+		`(write [])`,
+		`(write {})`,
+		`(write nil)`,
+		`(write [42 []])`,
+		`(write (read))`,
+	} {
+		m, err := parse.MainModule("<test>", s)
+		assert.Nil(t, err)
+
+		for _, s := range m {
+			for _, s := range desugarEmptyCollection(s) {
+				ast.Convert(func(x interface{}) interface{} {
+					a, ok := x.(ast.App)
+
+					if !ok {
+						return nil
+					}
+
+					switch a.Function() {
+					case consts.Names.EmptyList, consts.Names.EmptyDictionary:
+						assert.NotZero(t, len(a.Arguments().Positionals()))
+					}
+
+					return nil
+				}, s)
+			}
+		}
+	}
+}

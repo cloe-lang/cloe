51442ba779becaa831ac315b51be2d32e030dd1c
Yota Toyama <raviqqe@gmail.com>
2017-12-11 09:43:17 +0900

    Test environment object

---
+++ src/lib/compile/environment_test.go
@@ -0,0 +1,33 @@
+package compile
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tisp-lang/tisp/src/lib/core"
+)
+
+func testFallback(s string) (*core.Thunk, error) {
+	return nil, errors.New("name not found")
+}
+
+func TestNewEnvironment(t *testing.T) {
+	newEnvironment(testFallback)
+}
+
+func TestEnvironmentGetFail(t *testing.T) {
+	defer func() {
+		_, ok := recover().(error)
+		assert.True(t, ok)
+	}()
+
+	newEnvironment(testFallback).get("foo")
+}
+
+func TestEnvironmentGet(t *testing.T) {
+	e := newEnvironment(testFallback)
+	e.set("foo", core.Nil)
+	th := e.get("foo")
+	assert.Equal(t, core.Nil, th)
+}

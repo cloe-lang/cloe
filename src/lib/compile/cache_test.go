d2152bc3480daef653d5d98ca4de737eeaf0a6b4
Yota Toyama <raviqqe@gmail.com>
2017-12-11 11:44:38 +0900

    Increase test coverange of compile/cache.go

--- src/lib/compile/cache_test.go
+++ src/lib/compile/cache_test.go
@@ -1,6 +1,8 @@
 package compile
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,6 +14,13 @@ func TestNewModulesCache(t *testing.T) {
 
 func TestModulesSet(t *testing.T) {
 	assert.Nil(t, newModulesCache().Set("foo", nil))
+}
+
+func TestModulesSetInNonExsitentDirectory(t *testing.T) {
+	inNonExistentDirectory(t, func() {
+		err := newModulesCache().Set("foo", nil)
+		assert.NotNil(t, err)
+	})
 }
 
 func TestModulesGet(t *testing.T) {
@@ -26,4 +35,26 @@ func TestModulesGet(t *testing.T) {
 	assert.Equal(t, module(nil), m)
 	assert.True(t, ok)
 	assert.Nil(t, err)
+}
+
+func TestModulesGetInNonExsitentDirectory(t *testing.T) {
+	inNonExistentDirectory(t, func() {
+		_, _, err := newModulesCache().Get("foo")
+		assert.NotNil(t, err)
+	})
+}
+
+func inNonExistentDirectory(t *testing.T, f func()) {
+	d, err := ioutil.TempDir("", "")
+	assert.Nil(t, err)
+
+	wd, err := os.Getwd()
+	assert.Nil(t, err)
+
+	os.Chdir(d)
+	os.Remove(d)
+
+	f()
+
+	os.Chdir(wd)
 }

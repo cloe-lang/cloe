975e9ab7a8f6e3d644d6c5a93e8c9e22d0aac46c
Yota Toyama <raviqqe@gmail.com>
2017-12-11 09:06:04 +0900

    Test effect.go

---
+++ src/lib/compile/effect_test.go
@@ -0,0 +1,21 @@
+package compile
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tisp-lang/tisp/src/lib/core"
+)
+
+func TestNewEffect(t *testing.T) {
+	t.Log(NewEffect(core.Nil, false))
+}
+
+func TestEffectValue(t *testing.T) {
+	assert.NotEqual(t, (*core.Thunk)(nil), NewEffect(core.Nil, false).Value())
+}
+
+func TestEffectExpanded(t *testing.T) {
+	assert.False(t, NewEffect(core.Nil, false).Expanded())
+	assert.True(t, NewEffect(core.EmptyList, true).Expanded())
+}

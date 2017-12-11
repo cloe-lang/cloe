6bded6895ca9ff5ab8e2e8b83bbffe8d19509199
Yota Toyama <raviqqe@gmail.com>
2017-12-11 10:28:41 +0900

    Test compile function with submodules

--- src/lib/compile/compile_test.go
+++ src/lib/compile/compile_test.go
@@ -1,29 +1,38 @@
 package compile
 
 import (
+	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/tisp-lang/tisp/src/lib/consts"
 )
 
 func TestCompile(t *testing.T) {
-	f, err := ioutil.TempFile("", "foo.tisp")
-	assert.Nil(t, err)
+	for _, s := range []string{
+		`(write "Hello, world!")`,
+		`(import "http") (write (http.get "http://httpbin.org"))`,
+	} {
+		f, err := ioutil.TempFile("", "")
+		assert.Nil(t, err)
 
-	f.WriteString(`(write "Hello, world!")`)
+		f.WriteString(s)
 
-	err = f.Close()
-	assert.Nil(t, err)
+		err = f.Close()
+		assert.Nil(t, err)
 
-	es, err := Compile(f.Name())
+		es, err := Compile(f.Name())
 
-	assert.Nil(t, err)
-	assert.Equal(t, 1, len(es))
+		assert.Nil(t, err)
+		assert.Equal(t, 1, len(es))
+	}
 }
 
 func TestCompileSourceOfInvalidSyntax(t *testing.T) {
-	f, err := ioutil.TempFile("", "foo.tisp")
+	f, err := ioutil.TempFile("", "")
 	assert.Nil(t, err)
 
 	f.WriteString(`(write "Hello, world!"`)
@@ -45,4 +54,38 @@ func TestCompileStdin(t *testing.T) {
 	es, err := Compile("")
 	assert.Nil(t, err)
 	assert.Zero(t, len(es))
+}
+
+func TestCompileWithSubModule(t *testing.T) {
+	// Module script
+
+	f, err := ioutil.TempFile("", "")
+	assert.Nil(t, err)
+
+	f.WriteString(`(def (hello name) (merge "Hello, " name "!"))`)
+
+	err = f.Close()
+	assert.Nil(t, err)
+
+	m := f.Name()
+	err = os.Rename(f.Name(), f.Name()+consts.FileExtension)
+	fmt.Println(f.Name())
+	assert.Nil(t, err)
+
+	// Main script
+
+	f, err = ioutil.TempFile("", "")
+	assert.Nil(t, err)
+
+	f.WriteString(fmt.Sprintf(`(import "%v") (write (%v.hello "John"))`, m, filepath.Base(m)))
+
+	err = f.Close()
+	assert.Nil(t, err)
+
+	// Compile main script
+
+	es, err := Compile(f.Name())
+
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(es))
 }

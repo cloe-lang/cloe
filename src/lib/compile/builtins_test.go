0a4b766395141c987d8bb19cb375eb64ae3e0ff1
Yota Toyama <raviqqe@gmail.com>
2017-12-11 09:53:28 +0900

    Increase test coverange of builtins.go

--- src/lib/compile/builtins_test.go
+++ src/lib/compile/builtins_test.go
@@ -1,7 +1,33 @@
 package compile
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestBuiltinsEnvironment(t *testing.T) {
 	builtinsEnvironment()
+}
+
+func TestCompileBuiltinModule(t *testing.T) {
+	compileBuiltinModule(newEnvironment(testFallback), "", `(def (foo x) x)`)
+}
+
+func TestCompileBuiltinModuleWithInvalidSyntax(t *testing.T) {
+	defer func() {
+		_, ok := recover().(error)
+		assert.True(t, ok)
+	}()
+
+	compileBuiltinModule(newEnvironment(testFallback), "", `(def (foo x) x`)
+}
+
+func TestCompileBuiltinModuleWithInvalidSource(t *testing.T) {
+	defer func() {
+		_, ok := recover().(error)
+		assert.True(t, ok)
+	}()
+
+	compileBuiltinModule(newEnvironment(testFallback), "", `(def (foo x) y)`)
 }

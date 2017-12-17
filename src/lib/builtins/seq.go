c2eb52849622696bfbfcfefd01ffe0baf3666d48
Yota Toyama <raviqqe@gmail.com>
2017-12-18 05:58:26 +0900

    Refactor seq.go

--- src/lib/builtins/seq.go
+++ src/lib/builtins/seq.go
@@ -6,22 +6,20 @@ import (
 
 // Seq runs arguments of effects sequentially.
 var Seq = core.NewLazyFunction(
-	core.NewSignature(
-		nil, nil, "effects",
-		nil, nil, "",
-	),
+	core.NewSignature(nil, nil, "effects", nil, nil, ""),
 	func(ts ...*core.Thunk) core.Value {
 		l := ts[0]
 
 		for {
-			out := core.PApp(core.First, l)
-			if err, ok := out.EvalEffect().(core.ErrorType); ok {
+			t := core.PApp(core.First, l)
+
+			if err, ok := t.EvalEffect().(core.ErrorType); ok {
 				return err
 			}
 
 			l = core.PApp(core.Rest, l)
 
-			if v := checkEmptyList(l, out); v != nil {
+			if v := checkEmptyList(l, t); v != nil {
 				return v
 			}
 		}

258ed21784f572c396a8ec22024a323f5403bea2
Yota Toyama <raviqqe@gmail.com>
2017-12-18 13:01:38 +0900

    Refactor par.go

--- src/lib/builtins/par.go
+++ src/lib/builtins/par.go
@@ -7,26 +7,18 @@ import (
 
 // Par evaluates arguments in parallel and returns the last one.
 var Par = core.NewLazyFunction(
-	core.NewSignature(
-		nil, nil, "xs",
-		nil, nil, "",
-	),
+	core.NewSignature(nil, nil, "xs", nil, nil, ""),
 	func(ts ...*core.Thunk) core.Value {
 		l := ts[0]
 
-		if v := checkEmptyList(l, core.NumArgsError("par", "> 0")); v != nil {
-			return v
-		}
-
 		for {
 			t := core.PApp(core.First, l)
-			systemt.Daemonize(func() {
-				t.Eval()
-			})
-
 			l = core.PApp(core.Rest, l)
+
 			if v := checkEmptyList(l, t); v != nil {
 				return v
 			}
+
+			systemt.Daemonize(func() { t.Eval() })
 		}
 	})

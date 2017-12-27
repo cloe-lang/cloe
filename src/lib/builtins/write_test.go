d67a7bfd735bde2f61892191cc7bad3aa59f1f21
Yota Toyama <raviqqe@gmail.com>
2017-12-27 11:29:31 +0900

    Test error cases of write function

--- src/lib/builtins/write_test.go
+++ src/lib/builtins/write_test.go
@@ -1,6 +1,8 @@
 package builtins
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/coel-lang/coel/src/lib/core"
@@ -27,9 +29,86 @@ func TestWrite(t *testing.T) {
 			nil),
 		core.NewArguments(
 			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
+			[]core.KeywordArgument{core.NewKeywordArgument("file", core.NewNumber(2))},
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
 			[]core.KeywordArgument{core.NewKeywordArgument("mode", core.NewNumber(0775))},
 			nil),
 	} {
 		assert.Equal(t, core.Nil.Eval(), core.App(Write, a).EvalEffect())
+	}
+}
+
+func TestWriteError(t *testing.T) {
+	d, err := ioutil.TempDir("", "")
+	assert.Nil(t, err)
+	defer os.Remove(d)
+
+	for _, a := range []core.Arguments{
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.NewError("", ""), false)},
+			nil,
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.NewError("", ""), true)},
+			nil,
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{
+				core.NewPositionalArgument(core.PApp(core.Prepend, core.Nil, core.NewError("", "")), true),
+			},
+			nil,
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
+			[]core.KeywordArgument{core.NewKeywordArgument("sep", core.Nil)},
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
+			[]core.KeywordArgument{
+				core.NewKeywordArgument("file", core.NewString("newFile")),
+				core.NewKeywordArgument("mode", core.Nil),
+			},
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
+			[]core.KeywordArgument{
+				core.NewKeywordArgument("file", core.NewError("", "")),
+			},
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
+			[]core.KeywordArgument{
+				core.NewKeywordArgument("file", core.True),
+			},
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
+			[]core.KeywordArgument{
+				core.NewKeywordArgument("file", core.True),
+			},
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
+			[]core.KeywordArgument{
+				core.NewKeywordArgument("file", core.NewString("/dev/full")),
+			},
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
+			[]core.KeywordArgument{
+				core.NewKeywordArgument("file", core.NewString("/dev/full")),
+			},
+			nil),
+		core.NewArguments(
+			[]core.PositionalArgument{core.NewPositionalArgument(core.Nil, false)},
+			[]core.KeywordArgument{
+				core.NewKeywordArgument("file", core.NewString(d)),
+			},
+			nil),
+	} {
+		_, ok := core.App(Write, a).Eval().(core.ErrorType)
+		assert.True(t, ok)
 	}
 }

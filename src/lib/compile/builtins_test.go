64054a546135cd718196baf7d03ee52ed69269bc
Yota Toyama <raviqqe@gmail.com>
2017-12-31 19:52:29 +0900

    Benchmark map function

--- src/lib/compile/builtins_test.go
+++ src/lib/compile/builtins_test.go
@@ -2,7 +2,11 @@ package compile
 
 import (
 	"testing"
+	"time"
 
+	"github.com/coel-lang/coel/src/lib/builtins"
+	"github.com/coel-lang/coel/src/lib/core"
+	"github.com/coel-lang/coel/src/lib/systemt"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -29,3 +33,50 @@ func TestCompileBuiltinModuleWithInvalidSource(t *testing.T) {
 
 	compileBuiltinModule(newEnvironment(testFallback), "", `(def (foo x) y)`)
 }
+
+func TestMapOrder(t *testing.T) {
+	b := func(N int) float64 {
+		var start time.Time
+		benchmarkMap(N, func() { start = time.Now() }, t.Fail)
+		return time.Since(start).Seconds()
+	}
+
+	r := b(10000) / b(2000)
+	t.Log(r)
+	assert.True(t, 4 < r && r < 6)
+}
+
+func BenchmarkMap(b *testing.B) {
+	benchmarkMap(b.N, b.ResetTimer, b.Fail)
+}
+
+func benchmarkMap(N int, startTimer, fail func()) {
+	go systemt.RunDaemons()
+
+	th := core.PApp(
+		builtinsEnvironment().get("$map"),
+		identity,
+		core.PApp(core.PApp(builtins.Y, many42)))
+
+	startTimer()
+
+	for i := 0; i < N; i++ {
+		if core.NumberType(42) != core.PApp(core.First, th).Eval().(core.NumberType) {
+			fail()
+		}
+
+		th = core.PApp(core.Rest, th)
+	}
+}
+
+var identity = core.NewLazyFunction(
+	core.NewSignature([]string{"arg"}, nil, "", nil, nil, ""),
+	func(ts ...*core.Thunk) core.Value {
+		return ts[0]
+	})
+
+var many42 = core.NewLazyFunction(
+	core.NewSignature([]string{"me"}, nil, "", nil, nil, ""),
+	func(ts ...*core.Thunk) core.Value {
+		return core.PApp(core.Prepend, core.NewNumber(42), core.PApp(ts[0]))
+	})
